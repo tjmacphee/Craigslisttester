03b686d21f241c7dbe832b138eabe59ec3bedc0d
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2024-04-07 13:17:37 -0400

    :sparkles: Account creation complete

--- runescapetester/src/test/java/fgcuvladtyler/TestAccountCreation.java
+++ runescapetester/src/test/java/fgcuvladtyler/TestAccountCreation.java
@@ -24,6 +24,28 @@ import java.util.Set;
 public class TestAccountCreation {
     private WebDriver driver;
     private WebDriverWait wait;
+    private Login login;
+
+    public static class Login {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 
     @BeforeClass
     public void setUp() {
@@ -32,14 +54,18 @@ public class TestAccountCreation {
             props.load(input);
             String webdriverPath = props.getProperty("webdriver.chrome.driver");
             System.setProperty("webdriver.chrome.driver", webdriverPath);
+
+            login = new Login();
+            login.setUsername(props.getProperty("login.username"));
+            login.setPassword(props.getProperty("login.password"));
+
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     /*
@@ -58,7 +84,7 @@ public class TestAccountCreation {
 
         // Wait for email input
         WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("create-email")));
-        emailInput.sendKeys("test@gmail.com");
+        emailInput.sendKeys(login.getUsername());
 
         // Enter date of birth by finding MM, DD, YYYY inputs xpath
         WebElement monthInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(text(), 'MM')]")));
@@ -75,6 +101,40 @@ public class TestAccountCreation {
         // Click continue button once no longer disabled
         WebElement continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("registration-start-form--continue-button")));
         continueButton.click();
+
+        // Wait for user to enter verification code from email into terminal
+        System.out.println("Enter verification code from email: ");
+        String code = System.console().readLine();
+        // Enter verification code & submit
+        WebElement verificationInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("registration-verify-form--input-code")));
+        verificationInput.sendKeys(code);
+        continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("registration-verify-form-continue-button")));
+        continueButton.click();
+
+        // Wait for user to enter username into terminal
+        System.out.println("Enter username: ");
+        String username = System.console().readLine();
+        // Enter username & submit
+        WebElement usernameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("displayName")));
+        usernameInput.sendKeys(username);
+        continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("registration-account-name-form--continue-button")));
+        continueButton.click();
+
+        // enter password into input
+        WebElement passwordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+        WebElement confirmPasswordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("repassword")));
+        passwordInput.sendKeys(login.getPassword());
+        confirmPasswordInput.sendKeys(login.getPassword());
+
+        // Click continue button once no longer disabled
+        continueButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("registration-password-form--create-account-button")));
+        continueButton.click();
+
+        // Wait until h1 tag with "All done!" is visible
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(), 'All done!')]")));
+
+        // Finished
+        System.out.println("Account created successfully!");
     }
 
     @AfterClass
