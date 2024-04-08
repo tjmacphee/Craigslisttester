80ba5f7663a0e4de56d7dec3d7eb5d59c24d59b6
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2024-04-07 20:06:53 -0400

    Switch to iframe

--- runescapetester/src/test/java/fgcuvladtyler/TestCharacterCreation.java
+++ runescapetester/src/test/java/fgcuvladtyler/TestCharacterCreation.java
@@ -20,13 +20,19 @@ public class TestCharacterCreation extends TestAccountCreation {
         public void createNewUser() throws InterruptedException {
             driver.get("https://account.runescape.com/en-GB/game");
             Thread.sleep(5000);
+            // Find the iframe whose id attribute starts with 'cf-ch'
+            WebElement iframeElement = driver.findElement(By.cssSelector("iframe[id^='cf-ch']"));
+
+            // Switch to the iframe
+            driver.switchTo().frame(iframeElement);
+
             WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
 
-          // Check if the checkbox is selected, if not, select it
-          if (!checkbox.isSelected()) {
-            Thread.sleep(3000);
-            checkbox.click();
-          }
+            // Check if the checkbox is selected, if not, select it
+            if (!checkbox.isSelected()) {
+              Thread.sleep(3000);
+              checkbox.click();
+            }
 
             WebElement createNewButton = driver.findElement(By.cssSelector("button[data-testid='game-accounts-create-new-button']"));
             createNewButton.click();
