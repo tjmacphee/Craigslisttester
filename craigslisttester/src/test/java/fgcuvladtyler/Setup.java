3885b3b9d157837de2d85ea10f58f4920dc31456
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2024-04-09 17:21:29 -0400

    :construction: Add test classes

--- craigslisttester/src/test/java/fgcuvladtyler/Setup.java
+++ craigslisttester/src/test/java/fgcuvladtyler/Setup.java
@@ -3,7 +3,6 @@ package fgcuvladtyler;
 import java.util.Objects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
@@ -13,13 +12,13 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
-public class SetupTests {
-    public static WebDriver driver;
+public class Setup {
+    public WebDriver driver;
     public static WebDriverWait wait;
 
     @BeforeSuite
-    public static void setUp() {
-       try (FileInputStream input = new FileInputStream(Objects.requireNonNull(SetupTests.class.getClassLoader().getResource("config.properties")).getFile())) {
+    public void setUp() {
+       try (FileInputStream input = new FileInputStream(Objects.requireNonNull(Setup.class.getClassLoader().getResource("config.properties")).getFile())) {
             Properties props = new Properties();
             props.load(input);
             String webdriverPath = props.getProperty("webdriver.chrome.driver");
@@ -28,12 +27,8 @@ public class SetupTests {
             ex.printStackTrace();
         }
 
-        if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-cookies");
-            driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-        }
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
@@ -47,7 +42,7 @@ public class SetupTests {
     }
 
     @AfterSuite
-    public static void tearDown() {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
