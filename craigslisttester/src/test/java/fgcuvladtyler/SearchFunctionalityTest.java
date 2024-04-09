cb40c52c17d14e34d6f1168a714a5c134473e707
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2024-04-09 11:57:28 -0400

    :sparkles: Search test

--- craigslisttester/src/test/java/fgcuvladtyler/SearchFunctionalityTest.java
+++ craigslisttester/src/test/java/fgcuvladtyler/SearchFunctionalityTest.java
@@ -5,14 +5,18 @@ import org.testng.annotations.Test;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SearchFunctionalityTest extends SetupTests {
     
     @Test (priority = 2)
     public void testSearch() {
-        // driver.get("");
-        // sleep(5000);
+        WebElement searchField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@placeholder,'search craigslist')]")));
+        sleep(2000);
+        searchField.sendKeys("Macbook Air 13", Keys.ENTER);
+        sleep(5000);
 
-        // Assert.assertTrue(driver.getTitle().contains(""));
+        // TODO: Add test to verify search worked, e.g. check for search results that contain "Macbook Air 13"
     }
 }
