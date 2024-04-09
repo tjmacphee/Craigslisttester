321583c98b097cf4b0709e370d14d7302daf7b19
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2024-04-09 11:42:44 -0400

    :books: Docs

--- craigslisttester/src/test/java/fgcuvladtyler/HomePageTests.java
+++ craigslisttester/src/test/java/fgcuvladtyler/HomePageTests.java
@@ -8,6 +8,7 @@ public class HomePageTests extends SetupTests {
     @Test (priority = 1)
     public void verifyHomePageTitle() {
         driver.get("https://www.craigslist.com");
+        sleep(15000);
         // String title = driver.getTitle();
         // Assert.assertTrue(title.contains(""));
     }
