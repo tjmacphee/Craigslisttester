2d8942ec695c65d7884b90a5e77cb8cd742120c8
vladrph <vladimir.petithomme@hertz.com>
2024-04-09 22:23:04 -0400

    Adding implementation to check for account creation moved driver = new ChromeDriver(); from line 30 to line 16 to allow for global class access

--- craigslisttester/src/test/java/fgcuvladtyler/AccountCreation.java
+++ craigslisttester/src/test/java/fgcuvladtyler/AccountCreation.java
@@ -1,5 +1,18 @@
 package fgcuvladtyler;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+
 public class AccountCreation extends Setup {
-    
+
+  @Test
+  public void createAccount() {
+    System.out.println("Creating account...");
+    String url = "https://accounts.craigslist.org/login?rp=%2Flogin%2Fhome&rt=L";
+    driver.get(url);
+    driver.findElement(By.xpath("//*[@id=\"emailAddress\"]")).sendKeys("testemail@test.com");
+    driver.findElement(By.xpath("//*[@id=\"create\"]")).click();
+    sleep(2000);
+
+  }
 }
