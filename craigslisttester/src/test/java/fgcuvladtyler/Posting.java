d2e682e123308006fb843e4b177c6c341065dda7
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2024-04-13 12:00:24 -0400

    :sparkles: Profile class

--- craigslisttester/src/test/java/fgcuvladtyler/Posting.java
+++ craigslisttester/src/test/java/fgcuvladtyler/Posting.java
@@ -9,7 +9,6 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.JavascriptExecutor;
 
